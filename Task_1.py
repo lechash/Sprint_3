d2d631d3a8b5440edf033e8e3cdded3327fb731b
Lechash <lechash@yandex.ru>
2025-12-16 23:52:10 +0300

    исправлена синтаксическая ошибка в 20 строке

--- Task_1.py
+++ Task_1.py
@@ -17,7 +17,7 @@ class OnlineSalesRegisterCollector:
         return self.__number_items
 
     def add_item_to_cheque(self, name):
-        if len(name) = 0 or len(name) > 40:
+        if len(name) == 0 or len(name) > 40:
             raise ValueError('Нельзя добавить товар, если в его названии нет символов или их больше 40')
         if name not in self.__item_price:
             raise NameError('Позиция отсутствует в товарном справочнике')
